90f73eb4ebe1c05a959bb9bea3062637e1b86928
Mic Neale <michael.neale@gmail.com>
2024-02-08 20:40:22 +1100

    actually works

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://repo.danubetech.com/repository/maven-public/")
+
+    }
+}
+
+rootProject.name = "Tbdexy"
+include(":app")
+ 
